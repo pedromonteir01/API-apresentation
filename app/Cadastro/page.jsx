abc2621c481089887fe2f76e9bd956e6688fbc52
raissas2 <raissa.floriano@aluno.senai.br>
2023-10-19 17:21:33 -0300

    cadastro começando

--- app/Cadastro/page.jsx
+++ app/Cadastro/page.jsx
@@ -2,29 +2,30 @@
 import { useState } from "react";
 import Cadastro from "@/models/Cadastro";
 import Cadastros from "@/models/Cadastros";
+import style from '@/app/Cadastro/cadastro.module.css'
 import { Main } from "next/document";
 
 const cadastros = new Cadastros();
 
-function Times (){
+function Times() {
 
     const [aux, setAux] = useState(null);
-   
+
     let vazia = '';
-    const [nomeTime,setTime] = useState(vazia);
+    const [nomeTime, setTime] = useState(vazia);
 
     const [show, setShow] = useState(false);
 
-    const [lista, setLista] = useState (cadastros.lista);
+    const [lista, setLista] = useState(cadastros.lista);
 
     const showCadastros = () => {
-        if (nomeTime.trim() == ''){
+        if (nomeTime.trim() == '') {
             alert('testando')
-        }else{
+        } else {
             const cadastro = new Cadastro(nomeTime);
             cadastros.addCadastros(cadastro)
             setTime(vazia);
-         
+
             console.log(cadastros.lista);
         }
     }
@@ -37,7 +38,7 @@ function Times (){
     }
 
     const editCadastro = () => {
-        cadastros.editCadastro(aux, nomeTime);
+        cadastros.editarCadastro(aux, nomeTime);
 
         setTime(vazia);
         setLista(cadastros.lista);
@@ -52,7 +53,7 @@ function Times (){
             cadastro.id == id ? already = true : already
         ))
 
-        if(already){
+        if (already) {
             cadastros.deleteCadastros(id);
             setLista(cadastros.lista);
         }
@@ -60,54 +61,54 @@ function Times (){
 
     return (
         <main>
-        <div className={styles.mainContent}>
-        <h1>Cadastros</h1>
-        <article className={styles.timeform}>
-            <section className={styles.input}>
-                <label>Nome do Time:</label>
-                <input type="text" 
-                value={nomeTime}
-                onChange={(param) => {
-                 setTime(param.target.value);
-                }}
-                className={styles.infor} 
-               />
-            </section>
-            <section className={styles.div}>
-                {
-                    show && (
-                        <button onClick={editUser} className={styles.btn}>Editar</button>
-                    )
-                }
-                    { 
-                        ! show && (
-                    <button onClick={showCadastros} className={styles.btn}>Cadastrar</button>
-                        )
-                    }
-                
+            <div className={style.mainContent}>
+                <h1>Cadastros</h1>
+                <article className={style.timeform}>
+                    <section className={style.input}>
+                        <label>Nome do Time:</label>
+                        <input type="text"
+                            value={nomeTime}
+                            onChange={(param) => {
+                                setTime(param.target.value);
+                            }}
+                            className={style.infor}
+                        />
+                    </section>
+                    <section className={style.div}>
+                        {
+                            show && (
+                                <button onClick={editCadastro} className={style.btn}>Editar</button>
+                            )
+                        }
+                        {
+                            !show && (
+                                <button onClick={showCadastros} className={style.btn}>Cadastrar</button>
+                            )
+                        }
 
-            </section>
-        </article>
 
-        <article className={styles.cadastrolista}>
-            <h2>Lista</h2>
-            <section className={styles.seccadastros}>
-                {
-                    lista.map((cadastro) => (
-                        <div key={cadastro.id} className={style.cadastros}>
-                            <div className={style.lista}>
-                                <p><strong>Nome do time:</strong>{cadastro.nomeTime}</p>
-                            </div>
-                            <div className={style.btncadastros}>
-                                <button onClick={() => edit(cadastro.nomeTime, cadastro.id)} className={style.edit}>Editar</button>
-                                <button onClick={() => delet(cadastro.id)} className={style.delet}>Excluir</button>                            </div>
-                        </div>
-                    ))
-                }
+                    </section>
+                </article>
 
-            </section>
-        </article>
-        </div>
+                <article className={style.cadastrolista}>
+                    <h2>Lista</h2>
+                    <section className={style.seccadastros}>
+                        {
+                            lista.map((cadastro) => (
+                                <div key={cadastro.id} className={style.cadastros}>
+                                    <div className={style.lista}>
+                                        <p><strong>Nome do time:</strong>{cadastro.nomeTime}</p>
+                                    </div>
+                                    <div className={style.btncadastros}>
+                                        <button onClick={() => edit(cadastro.nomeTime, cadastro.id)} className={style.edit}>Editar</button>
+                                        <button onClick={() => delet(cadastro.id)} className={style.delet}>Excluir</button>                            </div>
+                                </div>
+                            ))
+                        }
+
+                    </section>
+                </article>
+            </div>
         </main>
     );
 }
